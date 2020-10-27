b086196e7fd720b7704efcfa39bd8f5315b4d4f5
Sione Tenifa <sionetenifa@Siones-MacBook-Pro.local>
2020-10-27 12:19:17 -0600

    functional

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 const todos = document.querySelector("#newList")
 const form  = document.querySelector("#todoForm")
 
-
 todos.addEventListener('click', function(e){
-    // console.log(e.target)
-    if(e.target.style.textDecoration == "line-through"){
-        e.target.style.textDecoration = ""
-    }else{
-        e.target.style.textDecoration = "line-through"
+    const lineRemove = e.target.tagName.toLowerCase()
+    // console.log(target)
+    switch(lineRemove){
+        case "li":
+            console.log("list was clicked")
+            if(e.target.style.textDecoration == "line-through"){
+                e.target.style.textDecoration = ""
+            }else {
+                e.target.style.textDecoration = "line-through"
+            }
+            break;
+        case "button":
+            console.log("Button was clicked")
+            e.target.parentNode.remove()
+            break;
     }
 })
 
 form.addEventListener("submit", function(e){
     e.preventDefault()
 
+    let remove = document.createElement("button")
+    remove.innerText = "REMOVE"
     let todo = document.createElement("li")
     todo.innerText = document.getElementById("list").value;
-    console.log(todo)
-
-    todos.appendChild(todo)
     
+
+
+    console.log(todo)
+    todos.appendChild(todo)
+    todo.appendChild(remove)
+    form.reset()
+
 } )
 
 
